99f50b90849f79eaa2fbe76c05e622d004521227
AVasilov <artur.vasilov@dz.ru>
2015-11-27 10:54:27 +0300

    started work with sensors

--- Training/app/src/main/java/ru/arturvasilov/training/sensors/MainActivity.java
+++ Training/app/src/main/java/ru/arturvasilov/training/sensors/MainActivity.java
@@ -1,15 +1,58 @@
 package ru.arturvasilov.training.sensors;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import ru.arturvasilov.training.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView mTextView;
+
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sensors_ac);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mTextView = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onStop() {
+        mSensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            StringBuilder builder = new StringBuilder("Light:")
+                    .append("\n");
+            for (float value : event.values) {
+                builder.append(value)
+                        .append("\n");
+            }
+            mTextView.setText(builder.toString());
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
