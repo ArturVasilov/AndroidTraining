9d47807d03c49f66afe1d6428501b7bb8b0c5060
Artur Vasilov <artur.vasilov@dz.ru>
2015-12-02 22:59:35 +0300

    add flavor dependency

--- Training/app/src/main/java/ru/arturvasilov/training/rx/MainActivity.java
+++ Training/app/src/main/java/ru/arturvasilov/training/rx/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import ru.arturvasilov.training.R;
+import rx.Observable;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just(1, 2, 3, 4, 5)
+                .compose(new Observable.Transformer<Integer, String>() {
+                    @Override
+                    public Observable<String> call(Observable<Integer> integerObservable) {
+                        return integerObservable.map(new Func1<Integer, String>() {
+                            @Override
+                            public String call(Integer integer) {
+                                return String.valueOf(integer);
+                            }
+                        });
+                    }
+                });
     }
 }
